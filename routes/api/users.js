70ebeaf8fa49a8c3a114eaa4289e2ca776ef1b22
Delagardi <delagardi@redy.io>
2018-10-29 03:50:09 +0200

    Added registration for uusers. Installed body-parser package

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+// Load User model
+const User = require('../../models/User');
+
 router.get('/test', (req, res) => res.json({
   message: "Users Test is working"
-}))
+}));
+
+router.post('/registration', (req, res) => {
+  // Using mongoose method findOne
+  User.findOne({
+    email: req.body.email
+  })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({
+          email: 'Sorry, email already exists'
+        });
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });
+
+        newUser.save()
+          .then(user => res.json(user))
+          .catch(error => console.log('We get some error with saving: ' + error));
+
+      }
+    })
+});
 
 module.exports = router;
