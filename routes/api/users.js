a1c34bee64786438e04ecf6deb277bc2060d4721
Delagardi <delagardi@redy.io>
2018-10-30 03:14:22 +0200

    Added login logic. Compare emails and hashed passwrods from DB

--- routes/api/users.js
+++ routes/api/users.js
@@ -50,4 +50,30 @@ router.post('/registration', (req, res) => {
     })
 });
 
+router.post('/login', (req, res) => {
+  User
+    .findOne({
+      email: req.body.email
+    })
+    .then(user => {
+      if(!user) {
+        return res.status(404).json({
+          message: "User not found"
+        })
+      }
+
+      bcrypt.compare(req.body.password, user.password).then(isMatch => {
+        if (isMatch) {
+          res.json({
+            message: "Well done!"
+          });
+        } else {
+          res.status(400).json({
+            message: "Password is incorrect"
+          });
+        }
+      })
+    });
+});
+
 module.exports = router;
